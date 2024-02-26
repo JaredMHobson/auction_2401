cd30f5bfa6ed64274fba1957ff8098cd2a4dad7b
Jared Hobson <jaredmhobson@gmail.com>
2024-02-26 08:12:57 -0800

    Update instantiations and exists test in auction_spec

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -3,9 +3,14 @@ require './lib/auction'
 require './lib/attendee'
 
 RSpec.describe Auction do
+  let (:item1) { Item.new('Chalkware Piggy Bank') }
+  let (:item2) { Item.new('Bamboo Picture Frame') }
+  let (:attendee) { Attendee.new({name: 'Megan', budget: '$50'}) }
+  let (:auction) { Auction.new }
+
   describe '#initialize' do
     it 'exists' do
-      expect(Auction).to be_instance_of(Auction)
+      expect(auction).to be_instance_of(Auction)
     end
   end
 end
