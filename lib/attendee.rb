786c188892cfd297def4cea9f174f8cb387901a9
Jared Hobson <jaredmhobson@gmail.com>
2024-02-26 09:32:17 -0800

    Add items_bid_on attribute and bid_on method in attendee

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,8 +1,13 @@
 class Attendee
-  attr_reader :name, :budget
+  attr_reader :name, :budget, :items_bid_on
 
   def initialize(details)
     @name = details[:name]
     @budget = details[:budget].gsub(/\D/,'').to_i
+    @items_bid_on = []
+  end
+
+  def bid_on(item)
+    @items_bid_on << item
   end
 end
