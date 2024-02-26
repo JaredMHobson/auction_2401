4301c9653cc288a774185422e33c16ba706bd401
Jared Hobson <jaredmhobson@gmail.com>
2024-02-26 08:11:20 -0800

    Add test for name and budget in attendee_spec

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe Attendee do
     it 'exists' do
       expect(attendee).to be_instance_of(Attendee)
     end
+
+    it 'has a name' do
+      expect(attendee.name).to eq('Megan')
+    end
+
+    it 'has a budget' do
+      expect(attendee.budget).to eq('$50')
+    end
   end
 end
