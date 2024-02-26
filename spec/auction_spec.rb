b32874311477d8940490e9a4a0d7fcccf2a1ddd3
Jared Hobson <jaredmhobson@gmail.com>
2024-02-26 08:48:39 -0800

    Add test for unpopular_items in auction_spec

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -54,9 +54,27 @@ describe '5 items and 3 attendees' do
   let (:attendee3) { Attendee.new({name: 'Mike', budget: '$100'}) }
   let (:auction) { Auction.new }
 
-  describe '#initialize' do
-    it 'exists' do
-      expect(auction).to be_instance_of(Auction)
+  describe '#unpopular_items' do
+    it 'can return an array of items that have no bids' do
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      expect(auction.unpopular_items).to eq([item1, item2, item3, item4, item5])
+
+      item1.add_bid(attendee2, 20)
+
+      expect(auction.unpopular_items).to eq([item2, item3, item4, item5])
+
+      item1.add_bid(attendee1, 22)
+
+      expect(auction.unpopular_items).to eq([item2, item3, item4, item5])
+
+      item4.add_bid(attendee3, 50)
+
+      expect(auction.unpopular_items).to eq([item2, item3, item5])
     end
   end
 end
