0ccd5f94d899f281b15a333132783b7795206d2d
Jared Hobson <jaredmhobson@gmail.com>
2024-02-26 08:20:18 -0800

    Add test for item_names method in auction_spec

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -29,4 +29,16 @@ RSpec.describe Auction do
       expect(auction.items).to eq([item1, item2])
     end
   end
+
+  describe '#item_names' do
+    it 'can list the names of all its items' do
+      auction.add_item(item1)
+
+      expect(auction.item_names).to eq(['Chalkware Piggy Bank'])
+
+      auction.add_item(item2)
+
+      expect(auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
+    end
+  end
 end
