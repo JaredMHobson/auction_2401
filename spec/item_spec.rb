df2ac77c122ff183a12fb736f874fecf18e1531b
Jared Hobson <jaredmhobson@gmail.com>
2024-02-26 08:07:12 -0800

    Add test for name method in item_spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,9 +3,15 @@ require './lib/auction'
 require './lib/attendee'
 
 RSpec.describe Item do
+  let (:item1) { Item.new('Chalkware Piggy Bank') }
+
   describe '#initialize' do
     it 'exists' do
-      expect(Item).to be_instance_of(Item)
+      expect(item1).to be_instance_of(Item)
+    end
+
+    it 'has a name' do
+      expect(item1.name).to eq('Chalkware Piggy Bank')
     end
   end
 end
